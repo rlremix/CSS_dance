be2943c37a0e21644f4b016278f80b4f68d45a15
rlremix <rlremix218@gmail.com>
2023-07-27 00:47:18 +0500

    idk

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//получаем значение скролла страницы в пикселях
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
